0dba03e4b2141181eb59f8700811cbd740f1040a
gkumar-pandey <gautamshekhar078@gmail.com>
2024-07-05 09:58:10 +0530

    update

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,19 +5,19 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
   const handleSearch = useDebouncedCallback((term: string) => {
-    console.log('searching...', term);
-    const params = new URLSearchParams(searchParams)
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 300)
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -25,7 +25,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       <input
         onChange={(e) => {
-          handleSearch(e.target.value)
+          handleSearch(e.target.value);
         }}
         defaultValue={searchParams.get('query')?.toString()}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
